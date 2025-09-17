51ec7f84b8cfcf0067e5cb2580bd7fbb641a9948
Carlos Barrera <carlos.barreraacuna@gmail.com>
2025-09-17 15:50:14 -0500

    crud funcional productos

--- config/cors.php
+++ config/cors.php
@@ -21,8 +21,11 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
+        'http://localhost:3001',
         'http://127.0.0.1:3000',
+        'http://127.0.0.1:3001',
         'https://localhost:3000',
+        'https://localhost:3001',
     ],
 
     'allowed_origins_patterns' => [],
